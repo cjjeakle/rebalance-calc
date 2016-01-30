bc2a9e53a56a24bd8593b92b67b6241a41bd551f
Chris Jeakle <cjjeakle@umich.edu>
2016-01-29 22:43:38 -0800

    Significant code cleanup

--- static/js/main.js
+++ static/js/main.js
@@ -47,9 +47,12 @@ var exampleData = `
 }
 `
 
+
 /*
 Utility functions
+-----------------
 */
+
 
 function clearState() {
     viewModel.assetClasses([]);
@@ -62,7 +65,17 @@ function getStateAsUrl() {
         accounts: viewModel.accounts
     };
 
-    return window.location.pathname + "#" + encodeURI(ko.toJSON(portfolioData));
+    var stripExtraData = function (key, value) {
+        switch(key) {
+            case 'parentCollection':
+            case 'remainder':
+                return;
+            default:
+                return value;
+        }
+    }
+
+    return window.location.pathname + "#" + encodeURI(ko.toJSON(portfolioData, stripExtraData));
 }
 
 function saveState() {
@@ -120,80 +133,109 @@ function moveElementDown(observableArray, index) {
     }
 }
 
+function arrayOfNulls(length) {
+    return new Array(length).map(function() {})
+}
+
 
 /*
-Web App Classes
+Web app classes
+---------------
 */
+
+
 class assetClass {
-    constructor(name, allocation, notes) {
+    constructor(name, allocation, notes, parentCollection) {
         this.name = ko.observable(name);
-        this.allocation = ko.observable(allocation);
+        this.allocation = ko.observable(parseFloat(allocation));
+        this.remainder = this.readAllocation();
         this.notes = ko.observable(notes);
+        this.parentCollection = parentCollection;
 
         this.allocation.subscribe(computepercentAllocated);
-
-        this.name.subscribe(computeAssetAllocation);
         this.allocation.subscribe(computeAssetAllocation);
-        this.notes.subscribe(computeAssetAllocation);
     }
 
     remove() {
-        viewModel.assetClasses.remove(this);
+        this.parentCollection.remove(this);
     }
 
     readAllocation() {
         return this.allocation() ? this.allocation() : 0;
     }
 
+    resetRemainder() {
+        this.remainder = this.readAllocation() / 100 * viewModel.totalAccountBalance();
+    }
+
     getIndex() {
-        return viewModel.assetClasses.indexOf(this);
+        return this.parentCollection.indexOf(this);
     }
 
     moveUp() {
-        moveElementUp(viewModel.assetClasses, this.getIndex());
+        moveElementUp(this.parentCollection, this.getIndex());
     }
     moveDown(){
-        moveElementDown(viewModel.assetClasses, this.getIndex());
+        moveElementDown(this.parentCollection, this.getIndex());
+    }
+
+    toJSON() {
+        var noCircularReference = ko.toJS(this);
+        delete noCircularReference.parentCollection;
+        return noCircularReference;
     }
 }
 
 class account {
-    constructor(name, balance, notes) {
+    constructor(name, balance, notes, parentCollection) {
         this.name = ko.observable(name);
-        this.balance = ko.observable(balance);
+        this.balance = ko.observable(parseFloat(balance));
+        this.remainder = this.balance();
         this.notes = ko.observable(notes);
+        this.parentCollection = parentCollection;
 
         this.balance.subscribe(computeTotalAccountBalance);
-
-        this.name.subscribe(computeAssetAllocation);
         this.balance.subscribe(computeAssetAllocation);
-        this.notes.subscribe(computeAssetAllocation);
     }
 
     remove() {
-        viewModel.accounts.remove(this);
+        this.parentCollection.remove(this);
     }
 
     readBalance() {
         return this.balance() ? this.balance() : 0;
     }
 
+    resetRemainder() {
+        this.remainder = this.readBalance();
+    }
+
+
     getIndex() {
-        return viewModel.accounts.indexOf(this);
+        return this.parentCollection.indexOf(this);
     }
 
     moveUp() {
-        moveElementUp(viewModel.accounts, this.getIndex());
+        moveElementUp(this.parentCollection, this.getIndex());
     }
     moveDown(){
-        moveElementDown(viewModel.accounts, this.getIndex());
+        moveElementDown(this.parentCollection, this.getIndex());
+    }
+
+    toJSON() {
+        var noCircularReference = ko.toJS(this);
+        delete noCircularReference.parentCollection;
+        return noCircularReference;
     }
 }
 
 
 /*
-Web App Logic
+Web app view model and view model manipulation functions
+--------------------------------------------------------
 */
+
+
 function AppViewModel() {
     this.assetClasses = ko.observableArray([]);
     this.accounts = ko.observableArray([]);
@@ -211,29 +253,34 @@ function AppViewModel() {
 var viewModel = new AppViewModel();
 
 function addAssetClass(name, allocation, notes) {
-    viewModel.assetClasses.push(new assetClass(name, allocation, notes));
+    viewModel.assetClasses.push(new assetClass(name, allocation, notes, viewModel.assetClasses));
 }
 
 function addAccount(name, balance, notes) {
-    viewModel.accounts.push(new account(name, balance, notes));
+    viewModel.accounts.push(new account(name, balance, notes, viewModel.accounts));
 }
 
 function computepercentAllocated() {
     viewModel.percentAllocated(0);
     viewModel.assetClasses().forEach(function(assetClass){
-        viewModel.percentAllocated(viewModel.percentAllocated() + parseFloat(assetClass.readAllocation()));
+        viewModel.percentAllocated(viewModel.percentAllocated() + assetClass.readAllocation());
     });
 }
 
 function computeTotalAccountBalance() {
     viewModel.totalAccountBalance(0);
     viewModel.accounts().forEach(function(account){
-        viewModel.totalAccountBalance(viewModel.totalAccountBalance() + parseFloat(account.readBalance()));
+        viewModel.totalAccountBalance(viewModel.totalAccountBalance() + account.readBalance());
     });
 }
 
 
-// TODO: This function is a tangled mess of repeated code, state, and nested logic. Refactor into stateless functions.
+/*
+The asset allocation calculator
+-------------------------------
+*/
+
+
 function computeAssetAllocation() {
     // Only compute the asset allocation matrix if there exist both accounts and assets
     if (viewModel.accounts().length == 0 ||
@@ -246,67 +293,117 @@ function computeAssetAllocation() {
     var currentAccountIndex = 0;
     var currentAssetIndex = 0;
 
-    var currentAccountBalance = parseFloat(viewModel.accounts()[currentAccountIndex].readBalance());
-    var currentAccountRemainder = currentAccountBalance;
+    var currentAccount = viewModel.accounts()[currentAccountIndex];
+    var currentAsset = viewModel.assetClasses()[currentAssetIndex];
+    var allocationMatrix = createAllocationMatrix();
 
-    var currentAssetBalance = parseFloat(viewModel.assetClasses()[currentAssetIndex].readAllocation()) / 100 * viewModel.totalAccountBalance();
-    var currentAssetRemainder = currentAssetBalance;
-
-    var allocationMatrix = [[viewModel.accounts()[currentAccountIndex].name()]];
+    currentAccount.resetRemainder();
+    currentAsset.resetRemainder();
 
     for (;;) {
-        if(currentAccountRemainder < currentAssetRemainder) {
-            currentAssetRemainder -= currentAccountRemainder;
+        if(currentAccount.remainder < currentAsset.remainder) {
+            currentAsset.remainder -= currentAccount.remainder;
 
-            var allocationInfo = 'amount: $' + currentAccountRemainder.toFixed(2) + ' (' + (currentAccountRemainder / currentAccountBalance * 100).toFixed(2) + '%)';
-            var accountVector = allocationMatrix[allocationMatrix.length-1];
-            accountVector.push(allocationInfo);
+            var allocationInfo = accountAllocationDescription(currentAccount.remainder, currentAccount.readBalance());
+            allocationMatrix[currentAccountIndex][accountVectorDataIndex(currentAssetIndex)] = allocationInfo;
 
-            while ((accountVector.length - 1) < viewModel.assetClasses().length) {
-                accountVector.push('amount: $0 (0%)');
-            }
+            denoteAccountFullyAllocated(allocationMatrix[currentAccountIndex], currentAssetIndex);
 
             currentAccountIndex++;
             if(currentAccountIndex  == viewModel.accounts().length) {
                 break;
             }
 
-            var nextAccountVector = [viewModel.accounts()[currentAccountIndex].name()];
-            for (var depletedAssetClasses = 0; depletedAssetClasses < currentAssetIndex; depletedAssetClasses++) {
-                nextAccountVector.push('amount: $0 (0%)');
-            }
-            allocationMatrix.push(nextAccountVector);
-            
-            var currentAccountBalance = parseFloat(viewModel.accounts()[currentAccountIndex].readBalance());
-            var currentAccountRemainder = currentAccountBalance;
+            currentAccount = viewModel.accounts()[currentAccountIndex];
+            currentAccount.resetRemainder();
         } else {
-            currentAccountRemainder -= currentAssetRemainder;
+            currentAccount.remainder -= currentAsset.remainder;
 
-            var allocationInfo = 'amount: $' + currentAssetRemainder.toFixed(2) + ' (' + (currentAssetRemainder / currentAccountBalance * 100).toFixed(2) + '%)';
-            allocationMatrix[allocationMatrix.length-1].push(allocationInfo);
+            var allocationInfo = accountAllocationDescription(currentAsset.remainder, currentAccount.readBalance());
+            allocationMatrix[currentAccountIndex][accountVectorDataIndex(currentAssetIndex)] = allocationInfo;
+
+            denoteAssetFullyAllocated(allocationMatrix, currentAssetIndex);
 
             currentAssetIndex++;
             if (currentAssetIndex == viewModel.assetClasses().length) {
                 break;
             }
 
-            var currentAssetBalance = parseFloat(viewModel.assetClasses()[currentAssetIndex].readAllocation()) / 100 * viewModel.totalAccountBalance();
-            var currentAssetRemainder = currentAssetBalance;
+            currentAsset = viewModel.assetClasses()[currentAssetIndex];
+            currentAsset.resetRemainder();
         }
     }
-
-    viewModel.computedAllocationHeaders(['']);
-    viewModel.assetClasses().forEach(function(assetClass) {
-        viewModel.computedAllocationHeaders.push(assetClass.name());
-    });
 
     viewModel.computedAllocation(allocationMatrix);
 }
 
+function createAllocationMatrix() {
+    var numAccounts = viewModel.accounts().length;
+    var numAssetClasses = viewModel.assetClasses().length;
+
+    viewModel.computedAllocationHeaders(['']);
+    viewModel.assetClasses().forEach(function(assetClass) {
+        viewModel.computedAllocationHeaders.push(assetClass.name);
+    });
+
+    var matrix = arrayOfNulls(numAccounts);
+    for(var i = 0; i < matrix.length; i++) {
+        matrix[i] = (arrayOfNulls(accountVectorDataIndex(numAssetClasses)));
+        matrix[i][0] = viewModel.accounts()[i].name;
+    }
+
+    return matrix;
+}
+
+// Add 1 to account vector when accessing data.
+// This is to account for the title entry.
+function accountVectorDataIndex(index) {
+    return index + 1;
+}
+
+function denoteAccountFullyAllocated(accountVector) {
+    for(var i = 0; i < accountVector.length; i++) {
+        if(!accountVector[i]) {
+            accountVector[i] = accountAllocationDescription(0, 0);
+        }
+    }
+}
+
+function denoteAssetFullyAllocated(allocationMatrix, assetIndex) {
+    allocationMatrix.forEach(function(accountVector) {
+        if(!accountVector[accountVectorDataIndex(assetIndex)]) {
+            accountVector[accountVectorDataIndex(assetIndex)] = (accountAllocationDescription(0, 0));
+        }
+    });
+}
+
+function accountAllocationDescription(amountAllocated, accountBalance) {
+    return 'amount: $' 
+        + amountAllocated.toFixed(2) 
+        + ' (' 
+        + (accountBalance != 0 ? (amountAllocated / accountBalance * 100).toFixed(2) : 0) 
+        + '%)';
+}
+
+
+/*
+Subscribe to the addition and removal of assets and accounts
+(to update calculated values)
+------------------------------------------------------------
+*/
+
+
 viewModel.assetClasses.subscribe(computepercentAllocated);
-viewModel.assetClasses.subscribe(computeAssetAllocation);
 viewModel.accounts.subscribe(computeTotalAccountBalance);
+viewModel.assetClasses.subscribe(computeAssetAllocation);
 viewModel.accounts.subscribe(computeAssetAllocation);
+
+
+/*
+Apply bindings to the DOM
+-------------------------
+*/
+
 
 // Activates knockout.js
 ko.applyBindings(viewModel);
