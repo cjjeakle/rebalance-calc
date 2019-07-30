1915ac4d4fc548529d0c1d6ce48a7001fb5d0280
Chris Jeakle <cjjeakle@umich.edu>
2019-07-30 02:00:22 -0500

    Refactored accounts to use a reducer hierarchy

--- src/store/reducers/accountsReducer.ts
+++ src/store/reducers/accountsReducer.ts
@@ -1,13 +1,27 @@
-import * as ActionTypes from "../ActionTypes";
+import * as ActionTypes from "../types/accountTypes";
+import mapReducerToTaxCategory from "./wrapperReducer";
+import { combineReducers } from "redux";
 
-const initialState = {
-}
-
-export default function accountsReducer(
-  state = initialState,
-  action
-) {
+function accountClassReducer(
+  state: ActionTypes.IAccountCategoryState = [],
+  action: ActionTypes.AccountActionTypes
+): ActionTypes.IAccountCategoryState {
+  switch (action.type) {
+    case ActionTypes.ADD_ACCOUNT:
+      return [...state, action.account];
+    case ActionTypes.MOVE_ACCOUNT:
+      let newState = [...state.slice(0, action.prevIndex), ...state.slice(action.prevIndex + 1)]
+      return newState.splice(action.newIndex, 0, state[action.prevIndex]);
+    case ActionTypes.REMOVE_ACCOUNT:
+      return [...state.slice(0, action.index), ...state.slice(action.index + 1)];
     default:
       return state;
   }
 }
+
+const accountsReducer = combineReducers({
+  accountsRegular: mapReducerToTaxCategory<ActionTypes.AccountTaxTreatment>(accountClassReducer, "Regular"),
+  accountsTaxDeferred: mapReducerToTaxCategory<ActionTypes.AccountTaxTreatment>(accountClassReducer, "TaxDeferred"),
+  accountsTaxExempt: mapReducerToTaxCategory<ActionTypes.AccountTaxTreatment>(accountClassReducer, "TaxExempt")
+});
+export default accountsReducer;
