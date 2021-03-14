6a2bcd223f60b3fd55f8cfbfd216e8a0e3fa97d5
Chris Jeakle <cjjeakle@umich.edu>
2021-03-13 19:07:27 -0600

    Fine-tune drag handle UX
    * Switched from three bars to a more standard "vertical grip" icon
    * Always align the drag icon to the left side of the relevant div
    * Added a slight background color to accounts (to distinguish rows)

--- src/components/accounts/Account.tsx
+++ src/components/accounts/Account.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { FaBars } from "react-icons/fa";
+import { FaGripVertical } from "react-icons/fa";
 import { Dispatch } from "redux";
 import { connect } from "react-redux";
 
@@ -65,96 +65,100 @@ class Account extends React.Component<IAccountProps> {
     });
 
     return (
-      <div className="container-fluid form-group" style={{backgroundColor:this.props.alternateBackground ? "WhiteSmoke" : "#EDF5F5" }}>
-        <div className="row">
-          <div className="col-lg-3" style={{display:"flex", alignItems:"center", justifyContent:"center", marginBottom:".5em 0 .5em 0"}}>
-            <FaBars />&nbsp;&nbsp;
-            <div>
-              <div className="row">
-                <div className="col-lg-12">
-                  <input
-                      type="text" 
-                      className="form-control" 
-                      placeholder="Account Name" 
-                      value={account.name} 
-                      onChange={
-                        (e: React.ChangeEvent<HTMLInputElement>) => { 
-                          this.props.updateName(account.id, e.target.value);
+      <div style={{display:"flex"}}>
+        <div style={{display:"flex", alignItems:"center", justifyContent:"center", marginRight:"1rem"}}>
+          <FaGripVertical />
+        </div>
+        <div className="container-fluid form-group" style={{/*override form group margins*/margin:".5rem 0 .5rem 0", backgroundColor:this.props.alternateBackground ? "WhiteSmoke" : "#EDF5F5"}}>
+          <div className="row">
+            <div className="col-lg-3" style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+              <div>
+                <div className="row">
+                  <div className="col-lg-12">
+                    <input
+                        type="text" 
+                        className="form-control" 
+                        placeholder="Account Name" 
+                        value={account.name} 
+                        onChange={
+                          (e: React.ChangeEvent<HTMLInputElement>) => { 
+                            this.props.updateName(account.id, e.target.value);
+                          }
                         }
-                      }
-                    >
-                  </input>
+                      >
+                    </input>
+                  </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <div className="input-group">
-                    <select 
-                      className={("form-control" + (account.taxTreatment as string === undefined ? " is-invalid" : ""))}
-                      value={account.taxTreatment}
-                      onChange={
-                        (e: React.ChangeEvent<HTMLSelectElement>) => {
-                          this.props.updateTaxTreatment(account.id, e.target.value as AccountTaxTreatmentT);
+                <div className="row">
+                  <div className="col">
+                    <div className="input-group">
+                      <select 
+                        className={("form-control" + (account.taxTreatment as string === undefined ? " is-invalid" : ""))}
+                        value={account.taxTreatment}
+                        onChange={
+                          (e: React.ChangeEvent<HTMLSelectElement>) => {
+                            this.props.updateTaxTreatment(account.id, e.target.value as AccountTaxTreatmentT);
+                          }
                         }
-                      }
-                    >
-                      <option disabled selected value="">-- Select a Tax Category: --</option>
-                      <option value="regular">Taxable</option>
-                      <option value="deferred">Tax deferred</option>
-                      <option value="exempt">Tax exempt</option>
-                    </select>
-                    <div className="input-group-append">
-                      <span className="input-group-text">
-                        <Info
-                          id={("tax-info-" + account.id)}
-                          title="What to choose:"
-                          detail={
-                            <div>
-                              <p>
-                                <strong>Taxable</strong>
-                                <br/>
-                                These are run-of-the-mill brokerage and investment accounts.
-                                They have no special tax advantages.
-                              </p>
-                              <p>
-                                <strong>Tax deferred</strong>
-                                <br/>
-                                These accounts don't owe taxes until you withdraw, thereby limiting the negative aspects of dividends and other tax-inefficient forms of returns.
-                                Examples include traditional 401(k), 403(b), and IRA accounts.
-                              </p>
-                              <p>
-                                <strong>Tax exempt</strong>
-                                <br/>
-                                Assets in these accounts are never taxed (if used correctly).
-                                Examples include Roth accounts and HSAs.
-                              </p>
-                            </div>
-                          }>
-                        </Info>
-                      </span>
+                      >
+                        <option disabled selected value="">-- Select a Tax Category: --</option>
+                        <option value="regular">Taxable</option>
+                        <option value="deferred">Tax deferred</option>
+                        <option value="exempt">Tax exempt</option>
+                      </select>
+                      <div className="input-group-append">
+                        <span className="input-group-text">
+                          <Info
+                            id={("tax-info-" + account.id)}
+                            title="What to choose:"
+                            detail={
+                              <div>
+                                <p>
+                                  <strong>Taxable</strong>
+                                  <br/>
+                                  These are run-of-the-mill brokerage and investment accounts.
+                                  They have no special tax advantages.
+                                </p>
+                                <p>
+                                  <strong>Tax deferred</strong>
+                                  <br/>
+                                  These accounts don't owe taxes until you withdraw, thereby limiting the negative aspects of dividends and other tax-inefficient forms of returns.
+                                  Examples include traditional 401(k), 403(b), and IRA accounts.
+                                </p>
+                                <p>
+                                  <strong>Tax exempt</strong>
+                                  <br/>
+                                  Assets in these accounts are never taxed (if used correctly).
+                                  Examples include Roth accounts and HSAs.
+                                </p>
+                              </div>
+                            }>
+                          </Info>
+                        </span>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col text-center">
-                  <em>Account Balance: $</em>{ roundToHundredths(Object.keys(accountHoldings).reduce((sum, assetId) => { return sum + accountHoldings[assetId].balance }, 0)) }
+                <div className="row">
+                  <div className="col text-center">
+                    <em>Account Balance: $</em>{ roundToHundredths(Object.keys(accountHoldings).reduce((sum, assetId) => { return sum + accountHoldings[assetId].balance }, 0)) }
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg">
-            <div className="row">
-            { holdings }
+            <div className="col-lg">
+              <div className="row">
+              { holdings }
+              </div>
             </div>
-          </div>
-          <div className="col-lg-1 text-center" style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
-            <button 
-              className="btn btn-outline-danger"
-              onClick={ () => {this.props.remove(account.id)} }
-            >
-              X
-            </button>
+            <div className="col-lg-1 text-center" style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+              <button 
+                className="btn btn-outline-danger"
+                onClick={ () => {this.props.remove(account.id)} }
+              >
+                X
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/assets/Asset.tsx
+++ src/components/assets/Asset.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { FaBars } from "react-icons/fa";
+import { FaGripVertical } from "react-icons/fa";
 import { Dispatch } from "redux";
 import { connect } from "react-redux";
 
@@ -25,136 +25,138 @@ class Asset extends React.Component<IAssetProps> {
   render() {
     let asset = this.props.asset;
     return (
-      <div className="container-fluid form-group">
-        <div className="row justify-content-center">
-          <div style={{display:"flex", alignItems:"center", justifyContent:"center", margin:".5em 0 .5em 0"}}>
-            <FaBars />
-          </div>
-          <div className="col-lg-2">
-            <input 
-              type="text" 
-              className="form-control" 
-              placeholder="Asset Name" 
-              value={asset.name} 
-              onChange={
-                (e: React.ChangeEvent<HTMLInputElement>) => { 
-                  this.props.updateName(asset.id, e.target.value);
-                }
-              }
-            >
-            </input>
-          </div>
-          <div className="col-lg-2">
-            <div className="input-group">
-              <input
-                type="number"
-                className="form-control"
-                id="allocation"
-                min="0"
-                max="100"
-                step="0.01"
-                placeholder="Allocation"
-                value={asset.allocation}
+      <div style={{display:"flex"}}>
+        <div style={{display:"flex", alignItems:"center", justifyContent:"center", marginRight:"1rem"}}>
+          <FaGripVertical />
+        </div>
+        <div className="container-fluid form-group" style={{/*override form group margins*/margin:".5rem 0 .5rem 0", backgroundColor:"#F8F8F8"}}>
+          <div className="row justify-content-center">
+            <div className="col-lg-2">
+              <input 
+                type="text" 
+                className="form-control" 
+                placeholder="Asset Name" 
+                value={asset.name} 
                 onChange={
-                  (e: React.ChangeEvent<HTMLInputElement>) => {
-                    this.props.updateAllocation(asset.id, e.target.valueAsNumber);
+                  (e: React.ChangeEvent<HTMLInputElement>) => { 
+                    this.props.updateName(asset.id, e.target.value);
                   }
                 }
               >
               </input>
-              <div className="input-group-append">
-                <span className="input-group-text" id="allocation">
-                  %&nbsp;&nbsp;
-                  <Info
-                    id={("allocation-info-" + asset.id)}
-                    title="Asset Allocation"
-                    detail={
-                      <div>
-                        <p>
-                          Use this field to specify what % of your whole portfolio you'd like to allocate to this asset.
-                        </p>
-                      </div>
-                    }>
-                  </Info>
-                </span>
+            </div>
+            <div className="col-lg-2">
+              <div className="input-group">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="allocation"
+                  min="0"
+                  max="100"
+                  step="0.01"
+                  placeholder="Allocation"
+                  value={asset.allocation}
+                  onChange={
+                    (e: React.ChangeEvent<HTMLInputElement>) => {
+                      this.props.updateAllocation(asset.id, e.target.valueAsNumber);
+                    }
+                  }
+                >
+                </input>
+                <div className="input-group-append">
+                  <span className="input-group-text" id="allocation">
+                    %&nbsp;&nbsp;
+                    <Info
+                      id={("allocation-info-" + asset.id)}
+                      title="Asset Allocation"
+                      detail={
+                        <div>
+                          <p>
+                            Use this field to specify what % of your whole portfolio you'd like to allocate to this asset.
+                          </p>
+                        </div>
+                      }>
+                    </Info>
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-3">
-            <div className="input-group">
-              <select 
-                className={("form-control" + (asset.taxTreatment as string === undefined ? " is-invalid" : ""))}
-                value={asset.taxTreatment}
+            <div className="col-lg-3">
+              <div className="input-group">
+                <select 
+                  className={("form-control" + (asset.taxTreatment as string === undefined ? " is-invalid" : ""))}
+                  value={asset.taxTreatment}
+                  onChange={
+                    (e: React.ChangeEvent<HTMLSelectElement>) => {
+                      this.props.updateTaxTreatment(asset.id, e.target.value as AssetTaxTreatmentT);
+                    }
+                  }
+                >
+                  <option disabled selected value="">-- Select a Tax Category: --</option>
+                  <option value="regular">No special tax treatment</option>
+                  <option value="inefficient">Tax inefficient</option>
+                  <option value="advantaged">Tax advantaged</option>
+                </select>
+                <div className="input-group-append">
+                  <span className="input-group-text">
+                    <Info
+                      id={("tax-info-" + asset.id)}
+                      title="What to choose:"
+                      detail={
+                        <div>
+                          <p>
+                            <strong>No special tax treatment</strong>
+                            <br/>
+                            These are assets with no special tax considerations, they earn a return by growing in value.
+                            Examples include individual stocks and typical passive index funds.
+                            Such assets typically issue qualified dividends, relatively small non-qualified dividends, or no dividend at all.
+                            <br />
+                            <em>
+                              Pro tip: For optimal results, place assets in this category with the most growth potential toward the top of the asset list.
+                              That ensures such assets are more likely to be allocated to a tax-advantaged account.
+                            </em>
+                          </p>
+                          <p>
+                            <strong>Tax inefficient</strong>
+                            <br/>
+                            These assets primarily earn via non-qualified dividends.
+                            Examples include most bonds and REITs.
+                          </p>
+                          <p>
+                            <strong>Tax advantaged</strong>
+                            <br/>
+                            These assets are eligible for a tax credit on foreign tax, or those which are entirely tax-exempt. 
+                            Examples include ex-us stock funds and municipal bonds.
+                          </p>
+                        </div>
+                      }>
+                    </Info>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg">
+              <input
+                type="text" 
+                className="form-control" 
+                placeholder="Notes" 
+                value={asset.notes}
                 onChange={
-                  (e: React.ChangeEvent<HTMLSelectElement>) => {
-                    this.props.updateTaxTreatment(asset.id, e.target.value as AssetTaxTreatmentT);
+                  (e: React.ChangeEvent<HTMLInputElement>) => { 
+                    this.props.updateNotes(asset.id, e.target.value);
                   }
                 }
               >
-                <option disabled selected value="">-- Select a Tax Category: --</option>
-                <option value="regular">No special tax treatment</option>
-                <option value="inefficient">Tax inefficient</option>
-                <option value="advantaged">Tax advantaged</option>
-              </select>
-              <div className="input-group-append">
-                <span className="input-group-text">
-                  <Info
-                    id={("tax-info-" + asset.id)}
-                    title="What to choose:"
-                    detail={
-                      <div>
-                        <p>
-                          <strong>No special tax treatment</strong>
-                          <br/>
-                          These are assets with no special tax considerations, they earn a return by growing in value.
-                          Examples include individual stocks and typical passive index funds.
-                          Such assets typically issue qualified dividends, relatively small non-qualified dividends, or no dividend at all.
-                          <br />
-                          <em>
-                            Pro tip: For optimal results, place assets in this category with the most growth potential toward the top of the asset list.
-                            That ensures such assets are more likely to be allocated to a tax-advantaged account.
-                          </em>
-                        </p>
-                        <p>
-                          <strong>Tax inefficient</strong>
-                          <br/>
-                          These assets primarily earn via non-qualified dividends.
-                          Examples include most bonds and REITs.
-                        </p>
-                        <p>
-                          <strong>Tax advantaged</strong>
-                          <br/>
-                          These assets are eligible for a tax credit on foreign tax, or those which are entirely tax-exempt. 
-                          Examples include ex-us stock funds and municipal bonds.
-                        </p>
-                      </div>
-                    }>
-                  </Info>
-                </span>
-              </div>
+              </input>
             </div>
-          </div>
-          <div className="col-lg">
-            <input
-              type="text" 
-              className="form-control" 
-              placeholder="Notes" 
-              value={asset.notes}
-              onChange={
-                (e: React.ChangeEvent<HTMLInputElement>) => { 
-                  this.props.updateNotes(asset.id, e.target.value);
-                }
-              }
-            >
-            </input>
-          </div>
-          <div className="col-lg-1 text-center" style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
-            <button 
-              className="btn btn-outline-danger"
-              onClick={ () => {this.props.removeAsset(asset.id)} }
-            >
-              X
-            </button>
+            <div className="col-lg-1 text-center" style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+              <button 
+                className="btn btn-outline-danger"
+                onClick={ () => {this.props.removeAsset(asset.id)} }
+              >
+                X
+              </button>
+            </div>
           </div>
         </div>
       </div>
